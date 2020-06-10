ec8e9149c6573a198414070cda8870f25a30c894
dasnotsad <shiningcloud@live.cn>
2020-06-10 23:59:12 +0800

    delete delaymsg

--- dasnotsad-framework-kafka/src/main/java/net/dasnotsad/framework/kafka/producer/KafkaProducer.java
+++ dasnotsad-framework-kafka/src/main/java/net/dasnotsad/framework/kafka/producer/KafkaProducer.java
@@ -5,7 +5,6 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 import lombok.extern.slf4j.Slf4j;
-import net.dasnotsad.framework.kafka.msg.model.DelayMessage;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
@@ -18,7 +17,6 @@ import com.alibaba.fastjson.JSONObject;
 
 import net.dasnotsad.framework.kafka.config.KafkaContainer;
 import net.dasnotsad.framework.kafka.msg.model.PushReturnType;
-import org.springframework.util.StringUtils;
 
 /**
  * kafka生产者操作类
@@ -168,109 +166,13 @@ public class KafkaProducer {
 		producer.flush();
 	}
 
-	/**
-	 * 延迟消息投递（同步）
-	 *
-	 * @param topic       主题
-	 * @param message     消息体
-	 * @param delayTime   延迟时间（秒）
-	 * @return
-	 */
-	public PushReturnType pushDelayMsgSync(String topic, Object message, long delayTime) {
-		return pushDelayMsgSync(topic, message, delayTime, kafkaContainer.get(whichDataSource.get()).getNodes());
-	}
-
-	/**
-	 * 延迟消息投递（同步）
-	 *
-	 * @param topic       主题
-	 * @param message     消息体
-	 * @param delayTime   延迟时间（秒）
-	 * @param nodes       连接地址（数据源）
-	 * @return
-	 */
-	public PushReturnType pushDelayMsgSync(String topic, Object message, long delayTime, String nodes) {
-		return pushDelayMsgSync(whichDataSource.get(), topic, message, delayTime, nodes);
-	}
-
-	/**
-	 * 延迟消息投递（同步）
-	 *
-	 * @param whichDataSource 数据源标识
-	 * @param topic       主题
-	 * @param message     消息体
-	 * @param delayTime   延迟时间（秒）
-	 * @param nodes       连接地址（数据源）
-	 * @return
-	 */
-	public PushReturnType pushDelayMsgSync(int whichDataSource, String topic, Object message, long delayTime, String nodes) {
-		DelayMessage model = new DelayMessage();
-		model.setTopic(topic);
-		model.setSys_code(applicationName);
-		model.setData(message);
-		model.setDelay_time(System.currentTimeMillis() + (delayTime * 1000));
-		model.setNodes(StringUtils.isEmpty(nodes) ? kafkaContainer.get(whichDataSource).getNodes(): nodes);
-		return pushMsgSync(whichDataSource, KAFKA_DELAY_TOPIC, null, model);
-	}
-
-	/**
-	 * 延迟消息投递（异步）
-	 *
-	 * @param topic       主题
-	 * @param message     消息体
-	 * @param delayTime   延迟时间（秒）
-	 * @param callBack 回调
-	 */
-	public void pushDelayMsgAsync(String topic, Object message, long delayTime, ProducerAsyncHandler callBack){
-		pushDelayMsgAsync(topic, message, delayTime, kafkaContainer.get(whichDataSource.get()).getNodes(), callBack);
-	}
-
-	/**
-	 * 延迟消息投递（异步）
-	 *
-	 * @param topic       主题
-	 * @param message     消息体
-	 * @param delayTime   延迟时间（秒）
-	 * @param nodes       连接地址（数据源）
-	 * @param callBack 回调
-	 */
-	public void pushDelayMsgAsync(String topic, Object message, long delayTime, String nodes, ProducerAsyncHandler callBack){
-		pushDelayMsgAsync(whichDataSource.get(), topic, message, delayTime, nodes, callBack);
-	}
-
-	/**
-	 * 延迟消息投递（异步）
-	 *
-	 * @param whichDataSource 数据源标识
-	 * @param topic       主题
-	 * @param message     消息体
-	 * @param delayTime   延迟时间（秒）
-	 * @param nodes       连接地址（数据源）
-	 * @param callBack 回调
-	 */
-	public void pushDelayMsgAsync(int whichDataSource, String topic, Object message, long delayTime, String nodes, ProducerAsyncHandler callBack){
-		DelayMessage model = new DelayMessage();
-		model.setTopic(topic);
-		model.setSys_code(applicationName);
-		model.setData(message);
-		model.setDelay_time(System.currentTimeMillis() + (delayTime * 1000));
-		model.setNodes(StringUtils.isEmpty(nodes) ? kafkaContainer.get(whichDataSource).getNodes() : nodes);
-		pushMsgAsync(whichDataSource, KAFKA_DELAY_TOPIC, null, model, callBack);
-	}
-
 	//创建kafka消息
 	private ProducerRecord<String, byte[]> createProducerRecord(KafkaProducerFactory producerFactory, String topic, String key, Object message){
 		ProducerRecord<String, byte[]> producerRecord;
 		if (message instanceof String)
 			producerRecord = new ProducerRecord<>(topic, key==null ? (String)message : key,
 					((String) message).getBytes(Charset.defaultCharset()));
-		else if(message instanceof DelayMessage){//延迟队列特殊处理
-			DelayMessage delayMsg = (DelayMessage)message;
-			if(StringUtils.isEmpty(delayMsg.getNodes()))
-				delayMsg.setNodes(producerFactory.getNodes());//默认取当前数据源
-			producerRecord = new ProducerRecord<>(topic, JSONObject.toJSONString(delayMsg),
-					JSON.toJSONBytes(delayMsg));
-		}else
+		else
 			producerRecord = new ProducerRecord<>(topic, JSONObject.toJSONString(message),
 					JSON.toJSONBytes(message));
 		return producerRecord;
